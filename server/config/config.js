fd948448ffb9d881ff50c478868d01c83ffbd4e5
FOXPP2047 <43031898+FOXPP2047@users.noreply.github.com>
2021-03-16 20:27:53 -0700

    Delete server/config directory

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    connectionLimit : 10,
-    password : "*Dydals12",
-    //password : "bojapassowrd12",
-    user : "root",
-    database : "bojaDB",
-    host : 'localhost',
-    port : '3306'
-}

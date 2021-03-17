441370e6b50eb3e89444647ff24a809e4f375429
Yongmin Cho <qlxhdl55@gmail.com>
2021-03-16 20:41:49 -0700

    updating gitignore

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,9 @@
-moudle.exports = {
-  connectionLimit : 10,
-  password : *Dydals12,
-  user : "root"
-  database : "bojaDB",
-  host : 'localhost',
-  port : '3306'
+module.exports = {
+    connectionLimit : 10,
+    //password : "*Dydals12",
+    password : "bojapassowrd12",
+    user : "root",
+    database : "bojaDB",
+    host : 'localhost',
+    port : '3306'
 }
